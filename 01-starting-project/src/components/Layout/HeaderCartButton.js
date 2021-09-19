5172fbbeebba0ef791e1423b44098c855cdd6459
DanieldelaCueva <76701720+DanieldelaCueva@users.noreply.github.com>
2021-09-19 20:15:10 +0200

    19/09

--- 01-starting-project/src/components/Layout/HeaderCartButton.js
+++ 01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,31 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
 
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => currentNumber + item.amount, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump: ''}`;
+
+  const { items } = cartCtx;
+
+  useEffect(() => {
+    if(items.length === 0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => setBtnIsHighlighted(false), 300);
+
+    return () => clearTimeout(timer);
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onToggle}>
+    <button className={btnClasses} onClick={props.onToggle}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
